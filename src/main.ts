84313e284a9a4e4206ab3a161f9d56e18c4139a0
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2024-01-25 21:53:28 +0900

    Vuetifyインストール＆設定

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+registerPlugins(app)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import Vue from '@vitejs/plugin-vue'
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import ViteFonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +13,22 @@ export default defineConfig({
     outDir: 'docs',
   },
   plugins: [
-    vue(),
+    Vue({
+      template: { transformAssetUrls },
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+    Vuetify(),
+    Components(),
+    ViteFonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900',
+        }],
+      },
+    }),
   ],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
